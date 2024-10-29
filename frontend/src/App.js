8bfc8436736e80995692f5a3793cbb529a0cf2d8
mattcoco <maxleiev007@gmail.com>
2024-10-30 00:44:53 +0100

    2.7 OK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  return <h1>Componentes Electrónicos Castellón</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                src="/logo192.png"
+                alt="logo"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              Componentes Electrónicos Castellón
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
