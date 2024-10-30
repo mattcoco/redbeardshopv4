31c732ddbf766cef507c39950480bc83ad35b056
mattcoco <maxleiev007@gmail.com>
2024-10-30 12:28:55 +0100

    7.8 OK

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,22 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Lo que tenga que ver con el userSchema se ejecutará antes de guardar el usuario en la base de datos
+userSchema.pre("save", async function (next) {
+  // Si el password no ha sido modificado, no hacemos nada
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Si el password ha sido modificado, lo encriptamos
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
